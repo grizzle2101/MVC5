432e3606de463189a30a4fe76dd02f876f8f5606
Conor McGrath <conormcgrath123@gmail.com>
2018-04-01 19:40:18 +0100

    MVC in Action

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // Task 2 - Create Controller & Pass Data from Model to View
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!", id = 1};
+
+            //Pass the movie Object to View.
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    //Task 1 - Create CLR Objects in our Model
+    public class Movie
+    {
+        public int id {get; set;}
+        public string Name {get; set;}
+    }
+}
