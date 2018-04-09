99fad8ae6ffe034a3fb4459603c3a258859f040e
Conor McGrath <conormcgrath123@gmail.com>
2018-04-09 14:38:09 +0100

    Section 3 - Lecture 8 - Querying Objects

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -7,30 +7,36 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        //Task 1 - Import DBContext
+        protected ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //Task 2 - Use Context to Retrive CustomerList.
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
+        //Task 3 - Use Context to Retrive Customer Details.
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
