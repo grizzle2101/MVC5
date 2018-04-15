fe8db0d0435b24f9b22cadc6561b054883c78315
Conor McGrath <conormcgrath123@gmail.com>
2018-04-15 13:50:46 +0100

    Section 4  - Tutorial 7/8 - Editing Customers

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
 using Vidly.ViewModels;
+using System;
 
 namespace Vidly.Controllers
 {
@@ -38,23 +39,55 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
+        //Task 2 - Edit Customer Action
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
-        //Task 2 - Implement Create Controller
+        //Task 4 - Refactoring Create to Save.
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            //Task 3 - Adding Customer to DB
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDB = _context.Customers.Single(c => c.Id == customer.Id);
+                //Method 1 - MVC Approved Way:
+                //TryUpdateModel(customerInDB, "", new string[] { "Name", "Email"});
+
+                //Method 2 - Manually Changing Properties
+                customerInDB.Name = customer.Name;
+                customerInDB.BirthDate = customer.BirthDate.Value;
+                customerInDB.MembershipTypeId = customer.MembershipTypeId;
+                customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+                //Method 3 - AutoMapper Way
+                //Mapper.Map(UpdateCustomerDTO, customerInDB);
+
+            }
             _context.SaveChanges();
             return RedirectToAction("Index","Customers");
         }
