5132b3303ca785b393f15ac569bd1332ff4a7304
Conor McGrath <conormcgrath123@gmail.com>
2018-06-01 02:02:39 +0100

    Section 7 - Tutorial 7 -  Returning Hierarchical Data

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            //Task 4 - Add Membership Mappings
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    //Task 3 - Add MembershipDTO to CustomerDTO
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        public MembershipTypeDto MembershipType { get; set; }
+
+//        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
